7b1c7f64a378a8bc3ea8e5e876ae8157ce11cc66
LB <larrybarrett11@icloud.com>
2024-11-19 21:51:51 -0600

    Read-in and concatenated several spreadsheet in to one dataframe, removed rows and columns that were used for formating within each spreadsheet, cleaned white space from column names and made each name lowercase, pasered out officers names from one column into several for 'first', 'last' and 'middle' etc.

---
+++ preprocess/clean/KS/import/src/import.py
@@ -0,0 +1,29 @@
+# Authors: LB
+# Mainrainers: LB
+# Copyright:   2023, HRDAG, GPL v2 or later
+# =========================================
+# us-post-data/preprocess/clean/KS/import/src
+import pandas as pd
+from pathlib import Path
+from nameparser import HumanName
+
+
+def clean_names(df):
+    out = df
+    hn = out.officer_name.apply(HumanName)
+    out['last_name']      = hn.apply(lambda x: x.last)
+    out['first_name']     = hn.apply(lambda x: x.first)
+    out['middle_name']    = hn.apply(lambda x: x.middle)
+    out['middle_initial'] = hn.apply(lambda x: x.middle[:1])
+    out['suffix']         = hn.apply(lambda x: x.suffix)
+    return out
+
+if __name__ == "__main__":
+    data_input = Path("../input/")
+    whole =  pd.concat(pd.read_excel(x,skiprows=4,usecols="c:k")
+                       for x in data_input.glob("*.xls"))
+    whole.columns  =  whole.columns.str.lower()
+    whole.columns = whole.columns.str.replace(" ", "_")
+    new_complete = clean_names(whole)
+    new_complete.drop(["unnamed:_3","unnamed:_4"], axis=1, inplace=True)
+    new_complete.head()
